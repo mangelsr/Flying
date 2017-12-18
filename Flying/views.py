29533f3acc10990f4b50bced6a436310e42eeea2
Miguel Sanchez <mangelsr@live.com>
2017-12-18 10:32:25 -0500

    [FIX] Imports en urls, y en views

--- Flying/views.py
+++ Flying/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
 	if request.method == 'GET':
-		return redner('formulario.html')
+		return render(request, 'formulario.html')
 	elif request.method == 'POST':
 		#TODO logica del query
 		context = {}
-		return render('formulario.html', context)
-	else
-		return httpresponse('Metodo no soportado')
+		return render(request, 'formulario.html', context)
+	else:
+		return HttpResponse('Metodo no soportado', content_type='text/plain; charset=utf8')
