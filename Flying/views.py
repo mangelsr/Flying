b3d2c31290621e1e23a472513a40c164e833c244
Miguel Sanchez <mangelsr@live.com>
2017-12-18 10:21:56 -0500

    [ADD] Ruta y vista basada en función para el formulario

--- Flying/views.py
+++ Flying/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+	if request.method == 'GET':
+		return redner('formulario.html')
+	elif request.method == 'POST':
+		#TODO logica del query
+		context = {}
+		return render('formulario.html', context)
+	else
+		return httpresponse('Metodo no soportado')
