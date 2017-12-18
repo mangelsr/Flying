963192ba04528bcff333f1445a9679d3adb78779
Alexis Rivadeneira <alexisleo15@gmail.com>
2017-12-18 11:34:55 -0500

    [upd] forms

--- Flying/forms.py
+++ Flying/forms.py
@@ -1,7 +1,20 @@
 from django import forms
 
 class Form(forms.Form):
-    origen = forms.CharField()
-    destino = forms.CharField()
-    directo = forms.ChoiceField(widget=forms.RadioSelect())
-    escala = forms.ChoiceField(widget=forms.RadioSelect())
+	def verPaises():
+		paises = []
+		f = open("countries.dat","r")
+		for line in f:
+			pais = line.split(",")[0]
+			paises.append(pais)
+		f.close()
+		return paises
+		
+	PAISES = verPaises()
+	CHOICES = (('directo','Vuelo directo'),('escala','Vuelo con escalas'))
+	origen = forms.CharField(widget=forms.Select(), choices=PAISES)
+	destino = forms.CharField(widget=forms.Select(), choices=PAISES)
+	radio = forms.ChoiceField(widget=forms.RadioSelect(), choices=CHOICES, label='')
+
+	
+
