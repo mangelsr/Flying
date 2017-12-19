6180799361afbfb018a7fe081919f2815487e45a
Miguel Sanchez <mangelsr@live.com>
2017-12-18 22:30:29 -0500

    [ADD] Funcionalidad de filtrado sin considerar numero de escalas

--- Flying/forms.py
+++ Flying/forms.py
@@ -1,20 +1,18 @@
 from django import forms
 
 class Form(forms.Form):
-	def verPaises():
+	def ver_paises():
 		paises = []
-		f = open("countries.dat","r")
+		f = open('countries.dat', 'r')
 		for line in f:
-			pais = line.split(",")[0]
-			paises.append(pais)
+			line = line.split(',')
+			paises.append((line[0], line[0]))
 		f.close()
 		return paises
 		
-	PAISES = verPaises()
+	PAISES = ver_paises()
 	CHOICES = (('directo','Vuelo directo'),('escala','Vuelo con escalas'))
-	origen = forms.CharField(widget=forms.Select(), choices=PAISES)
-	destino = forms.CharField(widget=forms.Select(), choices=PAISES)
+	origen = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), choices=PAISES)
+	destino = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), choices=PAISES)
 	radio = forms.ChoiceField(widget=forms.RadioSelect(), choices=CHOICES, label='')
-
-	
 
--- Flying/views.py
+++ Flying/views.py
@@ -1,15 +1,66 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import Form
 
 
-def verVuelos(request):
-	if request.method == "POST":
+def cargarAerolineas():
+	aerolineas = {}
+	f = open('airlines.dat', 'r')
+	for line in f:
+		line = line.split(',')
+		aerolineas[line[0]] = {'nombre': line[1].replace('"', ''), 
+			'iata': line[3].replace('"', ''), 'pais': line[6].replace('"', '')}
+	f.close()
+	return aerolineas
+
+def cargarAeropuertos():
+	airports = {}
+	f = open('airports.dat', 'r')
+	for line in f:
+		line = line.split(',')
+		if line[3].replace('"', '') not in airports:
+			airports[line[3].replace('"', '')] = [line[0]]
+		else:
+			airports[line[3].replace('"', '')].append(line[0])
+	f.close()
+	return airports
+
+def cargarResultados(origen, destino):
+	airports = cargarAeropuertos()
+
+	source_airports = airports[origen]
+	destination_airport = airports[destino]
+
+	airlines_id = set()
+	f = open('routes.dat', 'r')
+	for line in f:
+		line = line.split(',')
+		if (line[3] in source_airports) and (line[5] in destination_airport):
+			airlines_id.add(line[1])
+	f.close()
+
+	airlines = cargarAerolineas()
+
+	resultados = []
+	for air_id in airlines_id:
+		resultados.append(airlines[air_id])
+
+	return resultados
+
+
+def index(request):
+	if request.method == 'POST':
+		context = {}
 		form = Form(request.POST)
 		if form.is_valid():
-			vuelos = form.save(commit=False)
-			vuelos.save()
-		return redirect('formulario.html')
-	elif request.method == "GET":
+			origen = request.POST.get('origen')
+			destino = request.POST.get('destino')
+			context['form'] = form
+			context['lista_resultados'] = cargarResultados(origen, destino)
+		return render(request, 'formulario.html', context)
+	elif request.method == 'GET':
 		form = Form()
-		return render(request,'formulario.html', {'form':form})
+		return render(request,'formulario.html', {'form': form})
+	else:
+		return HttpResponse('Metodo no soportado')
 		
