ee49fb72fa9072b1226538884a38f351bfdb9e41
Miguel Sanchez <mangelsr@live.com>
2018-01-10 13:28:37 -0500

    [UPD] Fix mongodb url

--- Flying/views.py
+++ Flying/views.py
@@ -12,7 +12,7 @@ from .forms import Form
 from .models import *
 
 
-client = MongoClient('locamongodb://miguel:12345@ds245277.mlab.com:45277/flyinglhost')
+client = MongoClient('mongodb://miguel:12345@ds245277.mlab.com:45277/flyinglhost')
 db = client.flying
 
 
